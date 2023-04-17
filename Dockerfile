535a55fd69ce359706e7e20f45e5da3d5d6f54fd
mahmoud7mohamed <mahmoud11mohamed88@gmail.com>
2023-04-17 15:34:03 +0000

    success update

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,8 @@
-# FROM node:18-alpine
-# ENV NODE_ENV=production
-# WORKDIR /app
-# COPY ["package.json", "package-lock.json*", "./"]
-# RUN npm install --production
-# COPY . .
-# EXPOSE 3000
-# CMD ["node", "index.js"]
-
-FROM node:14-alpine
+FROM node:18-alpine
+ENV NODE_ENV=production
 WORKDIR /app
-COPY index.js package*.json ./
-RUN npm install
+COPY ["package.json", "package-lock.json*", "./"]
+RUN npm install --production
 COPY . .
-EXPOSE 3000
-CMD node index.js
+EXPOSE 5000
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
 const app = express();
 const Students = ["Ahmed", "Mahmoud", "Mohamed", "Hassan"];
-const Studentlist = (req, res) => {
-  let output = "<ul>";
-  for (let i = 0; i < Students.length; i++) {
-    output += "<li>" + Students[i] + "</li>";
-  }
-  output += "</ul>";
-  res.send(output);
+const StudentList = (req, res) => {
+    let m = "<h1 style='text-align:center; padding:10px;'> بسم الله الرحمن الرحيم</h1>";
+    let sum;
+    let output = "<ul style='padding:15px;margin-left:28px; color:#ff5511'>";
+    for (let i = 0; i < Students.length; i++) {
+        output += "<li><h2>" + Students[i] + "</h2></li>";
+    }
+    output += "</ul>";
+    sum = m + output;
+    res.send(sum);
 };
-app.get("/students", Studentlist);
-app.listen(3000);
+app.get("/students", StudentList);
+app.listen(5000);
+
